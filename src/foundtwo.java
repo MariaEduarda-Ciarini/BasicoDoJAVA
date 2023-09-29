b4a9a57eb41dd6580ed1375a547ebcbdfd4fd9ea
Maria Eduarda Ciarini Moura <DudaCiarinii@gmail.com>
2023-09-29 01:04:36 -0300

    SUFOCO :angry:
    damn

---
+++ src/foundtwo.java
@@ -0,0 +1,10 @@
+public class foundtwo {
+    public static void main(String[] args) {
+        Movie mMovie = new Movie();
+        mMovie.name = "Breakfast at Tiffany's";
+        mMovie.Release_year = 1961;
+        mMovie.duration_minutes = 114;
+
+        System.out.println(mMovie);
+    }
+}
---
+++ src/notfound.java
@@ -0,0 +1,9 @@
+public class Movie {
+    String name;
+    int Release_year;
+    boolean included_in_the_plan;
+    double rating;
+    int total_reviews;
+    int duration_minutes;
+
+}
